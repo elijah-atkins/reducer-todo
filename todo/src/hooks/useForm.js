4f4e833e73db22699da9004f3d75f62be058719f
elijah-atkins <elijahorama@gmail.com>
2020-05-20 10:44:48 -0600

    added hooks to code... will adapt hook to work with reducers

---
+++ todo/src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (initialState, key, submitLogic) => {
+
+  const [values, setValues] = useLocalStorage(initialState, key);
+
+  const handleChanges = e => {
+
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const clearForm = () => {
+    setValues(initialState);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    submitLogic();
+  };
+
+  return [values, handleChanges, clearForm, handleSubmit];
+};
